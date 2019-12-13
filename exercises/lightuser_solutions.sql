31945fa9b95d64cd433b554e8c74172e3e9e0724
Avision Ho <avision.ho@education.gov.uk>
2019-12-13 12:13:45 +0000

    feat: update exercises with COALESCE() addition.
    This is to reflect the change made in the notebook.

--- exercises/lightuser_solutions.sql
+++ exercises/lightuser_solutions.sql
@@ -2,7 +2,7 @@
 -- DESCRIPTION: The purpose of this script is to provide exercises for the trainee to practice what 
 --				was covered in the accompanying `training_sql_lightuser.ipynb` notebook.
 
-USE [HEFE-AN-DEV];
+USE [AdventureWorks];
 
 /* Question 1: FILTER, GROUP BY and COUNT */
 -- Q.		Using the [Person].[Address] table and based on the material up to (4.), can you count the number of records for each city
@@ -15,6 +15,7 @@ WHERE [StateProvinceID] = 11
 GROUP BY [City]
 ORDER BY [City];
 
+
 /* Question 2: JOIN-ing tables together */
 -- Q.		Using the [Person].[Person] and [Person].[EmailAddress] tables, can you retrieve the top 100 records first names, last names and email addresses?
 -- A.		Please write your query below here.
@@ -25,6 +26,7 @@ SELECT TOP 100
 FROM [Person].[Person] AS table_person
 LEFT JOIN [Person].[EmailAddress] AS table_email
 	ON table_person.[BusinessEntityID] = table_email.[BusinessEntityID];
+
 
 /* Question 3: UNION-ing tables together */
 -- Q.		Based on what we covered so far, can you think of an alternative way of writing the below query from (6.)?
@@ -55,11 +57,13 @@ SELECT
 FROM [Person].[Address]
 WHERE [City] IN ('Phoenix', 'New York');
 
+
 /* Question 4: CAST-ing columns to a specified datatype */
 -- Q.		Using the GETDATE() function to fetch today's date and time, can you cast this to only get the date instead?
 -- Note:	A `SELECT` statement need not always end with `FROM <table_name>`
 -- A.		Please write your query below here.
 SELECT CAST(GETDATE() AS DATE);
+
 
 /* Question 5: Avoiding long and winding CASE WHEN statements */
 -- Q.		Using the `[Sales].[Currency]` as your look-up table, find a way to do what you aim to do below in (7.2) but without using `CASE WHEN` statements. 
@@ -87,6 +91,7 @@ FROM [Sales].[CurrencyRate] AS table_rate
 LEFT JOIN [Sales].[Currency] AS table_lookup
 	ON table_rate.[FromCurrencyCode] = table_lookup.[CurrencyCode];
 
+
 /* Question 6: Creating a new conditional column */
 -- Q.		Using the [Person].[CountryRegion] table, can you classify countries that you think are military regimes under a new column, [MilitaryRegimeFlag]?
 -- A.		Please write your query below.
@@ -97,6 +102,7 @@ SELECT [CountryRegionCode]
 		ELSE 0
 		END
 FROM [Person].[CountryRegion];
+
 
 /* Question 7: CAST-ing columns so they can be concatenated */
 -- Q.		Using the [Sales].[CreditCard] table and the `[ExpMonth]` and `[ExpYear]` columns, 
@@ -119,3 +125,25 @@ SELECT [CardType]
 		ELSE CAST(CAST([ExpYear] AS VARCHAR(4)) + '-' + CAST([ExpMonth] AS VARCHAR(2)) + '-01' AS DATE)
 		END
 FROM [Sales].[CreditCard];
+
+
+/* Question 8: Returning first non-NULL entry across multiple columns */
+-- Q.		Using the COALESCE() function, can you rewrite the below query?
+-- Note:	This exercise shows you a shorthand way of creating a new column that 
+--			takes the first non-NULL entry across several existing columns.
+SELECT [Name]
+    ,[ProductNumber]
+    ,[ProductSubcategoryID]
+    ,[ProductModelID]
+    ,[ProductId] = CASE
+        WHEN [ProductSubcategoryID] IS NULL THEN [ProductModelID]
+        ELSE [ProductSubcategoryID]
+        END
+FROM [Production].[Product];
+-- A.		Please write your query below here.
+SELECT [Name]
+    ,[ProductNumber]
+    ,[ProductSubcategoryID]
+    ,[ProductModelID]
+    ,[ProductId] = COALESCE([ProductSubcategoryID], [ProductModelID])
+FROM [Production].[Product];
